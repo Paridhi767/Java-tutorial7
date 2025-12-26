476ace241294555e279ab526bb78fa4efb94160c
Paridhi Adhikari <paridhiadhikari13@gmail.com>
2025-12-26 12:59:14 +0545

    Added student name

--- anyfile.java
+++ anyfile.java
@@ -3,3 +3,15 @@ package tutorial7;
 public class anyfile {
 
 }
+
+
+class myName{
+	public void namae() {
+		System.out.println("Paridhi Adhikari");
+	}
+
+
+public void studentName() {
+	System.out.println("Nikesh");
+}
+}
